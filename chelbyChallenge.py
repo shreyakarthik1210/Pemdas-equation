7eabf4091c4e472463e3439e3dfef08c849e4d6c
shreyakarthik1210 <52420053+shreyakarthik1210@users.noreply.github.com>
2019-07-03 15:39:33 -0500

    Add files via upload

---
+++ chelbyChallenge.py
@@ -0,0 +1,8 @@
+x = int(input("Please type in a number: "))
+y = int(input("Please type in a number: "))
+z = int(input("Please type in a number: "))
+squared = x*x
+divide = y/z
+subtract = squared-divide
+answer = subtract*3
+print(answer)
